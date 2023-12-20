680b52504fd1dae3bbf4177e362ba2d0bfa53a76
Karim Ouazzou <karimouazzou@gmail.com>
2023-12-20 09:38:43 +0100

    fix problem categories in add book

--- src/main/java/com/tekup/LibraryApp/controller/manager/book/BookController.java
+++ src/main/java/com/tekup/LibraryApp/controller/manager/book/BookController.java
@@ -20,7 +20,7 @@ public class BookController {
 
     @GetMapping("/manager/book/add")
     public String showAddBookForm(Model model) {
-        model.addAttribute("allCategories", categoryService.getAllCategoriesNames());
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "manager/book/add";
     }
 
